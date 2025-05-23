96c07ce102562e1e927551a2a56942cb624f5a2c
abhandare <aniketbhandare165@gmail.com>
2025-05-23 23:52:52 +0530

    added new actions

--- pythonSelenium/Explicitwaitdemo.py
+++ pythonSelenium/Explicitwaitdemo.py
@@ -2,9 +2,11 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(5)
+driver.implicitly_wait(2)
 # 5 seconds is max time out 2 seconds (3 seconds save)
 
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
@@ -20,6 +22,8 @@ driver.find_element(By.XPATH,"//img[@alt='Cart']").click()
 driver.find_element(By.XPATH,"//button[normalize-space()='PROCEED TO CHECKOUT']").click()
 driver.find_element(By.XPATH,"//input[@class='promoCode']").send_keys("rahulshettyacademy")
 driver.find_element(By.XPATH,"//button[normalize-space()='Apply']").click()
-print(driver.find_element(By.CLASS_NAME,'promoInfo').text)
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.presence_of_element_located((By.XPATH,"//span[@class='promoInfo']")))
+print(driver.find_element(By.CLASS_NAME,"promoInfo").text)
 
 
